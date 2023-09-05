012b8c680ed7f97006a0684d3050d6748b801ada
VanDany <psr10701@students.ephec.be>
2023-09-06 01:21:09 +0200

    ADD Units field in GridObject

--- Assets/_Scripts/Testing.cs
+++ Assets/_Scripts/Testing.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    [SerializeField] private GameObject gridDebugObjectPrefab;
-
-    private GridSystem gridSystem;
     private void Start()
-    {
-        gridSystem = new GridSystem(10, 10, 2f);
-        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    { 
        
     }
     private void Update()
     {
-        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+
     }
 }
