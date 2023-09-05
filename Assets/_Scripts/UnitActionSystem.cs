b04b1a7e523a3cfdff03e7a79aadf21d0bdd6154
VanDany <psr10701@students.ephec.be>
2023-09-05 16:35:25 +0200

    Add Unit selection

--- Assets/_Scripts/UnitActionSystem.cs
+++ Assets/_Scripts/UnitActionSystem.cs
@@ -6,15 +6,28 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnityActionSystem" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     private void Update()
     {
-        if (TryHandleUnitSelection()) return;
-
         if (Input.GetMouseButtonDown(0))
         {
+            if (TryHandleUnitSelection()) return;
             selectedUnit.Move(MouseWorld.GetPosition());
         }   
     }
@@ -26,10 +39,19 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
