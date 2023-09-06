9c2336bb11fb82844e1f972f90a18e40e2452802
VanDany <psr10701@students.ephec.be>
2023-09-06 22:42:15 +0200

    ADD MoveAction Logic

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
-{
-    [SerializeField] private Animator unitAnimator;
-    private Vector3 targetPosition;
+{   
     private GridPosition gridPosition;
+    private MoveAction moveAction;
     private void Awake()
     {
-        targetPosition = transform.position;
+        moveAction = GetComponent<MoveAction>();
     }
     private void Start()
     {
@@ -18,23 +17,6 @@ public class Unit : MonoBehaviour
     }
     private void Update()
     {
-        
-        float stoppingDistance = .1f;
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
-        {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            float moveSpeed = 4f;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-            float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-
-            unitAnimator.SetBool("isWalking", true);
-        }
-        else
-        {
-            unitAnimator.SetBool("isWalking", false);
-        }
         GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
         if (newGridPosition != gridPosition)
         {
@@ -42,8 +24,13 @@ public class Unit : MonoBehaviour
             gridPosition = newGridPosition;
         }
     }
-    public void Move(Vector3 _targetPosition)
+    public MoveAction GetMoveAction()
     {
-        targetPosition = _targetPosition;
+        return moveAction;
     }
+    public GridPosition GetGridPosition()
+    {
+        return gridPosition;
+    }
+
 }
