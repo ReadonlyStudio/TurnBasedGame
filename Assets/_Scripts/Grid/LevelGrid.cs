e409e214738b9527508af0c88c0687bea74c1923
ReadonlyStudio <readonlystudio@gmail.com>
2023-09-22 19:44:57 +0200

    ADD Generic GridSystem

--- Assets/_Scripts/Grid/LevelGrid.cs
+++ Assets/_Scripts/Grid/LevelGrid.cs
@@ -10,7 +10,7 @@ public class LevelGrid : MonoBehaviour
 
     public event EventHandler OnAnyUnitMovedGridPosition; 
     [SerializeField] private GameObject gridDebugObjectPrefab;
-    private GridSystem gridSystem;
+    private GridSystem<GridObject> gridSystem;
 
     private void Awake()
     {
@@ -22,7 +22,7 @@ public class LevelGrid : MonoBehaviour
         }
         Instance = this;
 
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem<GridObject>(10, 10, 2f, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
