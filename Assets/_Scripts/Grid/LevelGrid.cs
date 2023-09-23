c8e7f29f463ade93d1218619d5b8897b7ddaf161
ReadonlyStudio <readonlystudio@gmail.com>
2023-09-23 22:12:48 +0200

    ADD obstacles

--- Assets/_Scripts/Grid/LevelGrid.cs
+++ Assets/_Scripts/Grid/LevelGrid.cs
@@ -9,7 +9,13 @@ public class LevelGrid : MonoBehaviour
     public static LevelGrid Instance { get; private set; }
 
     public event EventHandler OnAnyUnitMovedGridPosition; 
+
     [SerializeField] private GameObject gridDebugObjectPrefab;
+
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float cellSize;
+
     private GridSystem<GridObject> gridSystem;
 
     private void Awake()
@@ -22,8 +28,12 @@ public class LevelGrid : MonoBehaviour
         }
         Instance = this;
 
-        gridSystem = new GridSystem<GridObject>(10, 10, 2f, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
+        gridSystem = new GridSystem<GridObject>(width, height, cellSize, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         //gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    }
+    private void Start()
+    {
+        Pathfinding.Instance.Setup(width, height, cellSize);
     }
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
     {
--- Assets/_Scripts/Pathfinding.cs
+++ Assets/_Scripts/Pathfinding.cs
@@ -10,6 +10,7 @@ public class Pathfinding : MonoBehaviour
     private const int MOVE_DIAGONAL_COST = 14;
 
     [SerializeField] private Transform gridDebugObjectPrefab;
+    [SerializeField] private LayerMask obstaclesLayerMask;
 
     private int width;
     private int height;
@@ -24,10 +25,39 @@ public class Pathfinding : MonoBehaviour
             return;
         }
         Instance = this;
+
+    }
+
+    public void Setup(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
         gridSystem = new GridSystem<PathNode>(10, 10, 2f,
             (GridSystem<PathNode> g, GridPosition gridPosition) => new PathNode(gridPosition));
+        
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                Vector3 worldPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
+                float raycastOffsetDistance = 5f;
+                if (Physics.Raycast(
+                    worldPosition + Vector3.down * raycastOffsetDistance,
+                    Vector3.up,
+                    raycastOffsetDistance * 2,
+                    obstaclesLayerMask
+                    ))
+                {
+                    GetNode(x, z).SetIsWalkable(false);
+                }
+            }
+        }
     }
+
     public List<GridPosition> FindPath(GridPosition startGridPosition, GridPosition endGridPosition)
     {
         List<PathNode> openList = new List<PathNode>();
@@ -71,6 +101,11 @@ public class Pathfinding : MonoBehaviour
             {
                 if (closedList.Contains(neighbourNode))
                 {
+                    continue;
+                }
+                if (!neighbourNode.IsWalkable())
+                {
+                    closedList.Add(neighbourNode);
                     continue;
                 }
                 int tentativeGCost = 
